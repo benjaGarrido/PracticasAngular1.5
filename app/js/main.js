148a29b2e51c00adae0782fca79364b9e4005bb6
Benjamín Garrido Barreiro <benjamin.garrido@gmail.com>
2016-10-12 20:06:18 +0200

    bgarrido - modificación para mostrar listado

--- app/js/main.js
+++ app/js/main.js
@@ -3,8 +3,10 @@ var angularApp = angular.module("SorteoPremios", []);
 angularApp.controller("PremioController", function ($scope) {
     "use strict";
     $scope.listaPremios = ["coche deportivo", "viaje a Japón", "crucero por el Caribe", "apartamento en la playa"];
-    $scope.elegirPremio = function () {
-        var premioElegido = Math.floor(Math.random() * $scope.listaPremios.length);
-        $scope.premio = $scope.listaPremios[premioElegido];
+    $scope.elegirPremios = function () {
+        var primerPremio = Math.floor(Math.random() * $scope.listaPremios.length)
+            , ultimoPremio = Math.floor(Math.random() * $scope.listaPremios.length);
+        $scope.premios = $scope.listaPremios.slice(Math.min(primerPremio, ultimoPremio), Math.max(primerPremio, ultimoPremio));
+        $scope.fechaSorteo = new Date();
     }
 })
